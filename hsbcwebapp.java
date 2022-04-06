ec81c56af76264bd7230604c4a1a4874364add4e
georgeolunwa <georgeolunwa@gmail.com>
2022-04-06 19:41:58 +0200

    feature 302 changes by vivek

--- hsbcwebapp.java
+++ hsbcwebapp.java
@@ -28,4 +28,17 @@ public class FunctionCall {
 	System.out.println ("Inside funct2 with param " + param);
 	return param * 2;
     }
+	#feature302 changes by vivek
+	JNIEXPORT void JNICALL
+Java_Callbacks_nativeMethod(JNIEnv *env, jobject obj, jint depth)
+{
+    jclass cls = (*env)->GetObjectClass(env, obj);
+    jmethodID mid = (*env)->GetMethodID(env, cls, "callback", "(I)V");
+    if (mid == 0) {
+        return;
+    }
+    printf("In C, depth = %d, about to enter Java\n", depth);
+    (*env)->CallVoidMethod(env, obj, mid, depth);
+    printf("In C, depth = %d, back from Java\n", depth);
+}
 }
